d02f094683da0b9975fb292834a95b45e4c21c2d
TheaSophie <thea.engler@t-online.de>
2018-09-03 17:02:05 +0200

    test

--- makeModel/functions_includeSusyhit.py
+++ makeModel/functions_includeSusyhit.py
@@ -100,9 +100,9 @@ def PlottingTanBr(tanB, r, tanB2, r2, XERR, saveTitle):
 
 def PlottingTanBr2_includingHiggs(tanB, r, saveTitle):
     fig, ax = pyplot.subplots()
-    pyplot.plot(tanB, r, color='blue', marker='o', markersize=8, linestyle='dashed', linewidth=1)
+    pyplot.plot(tanB, r, color='blue', marker='o', markersize=7, linestyle='dashed', linewidth=1)
     yerr2 = r*87/1000
-    ax.errorbar(tanB, r, yerr=yerr2, color='blue', barsabove='False', linestyle='none', linewidth=0.5) 
+    ax.errorbar(tanB, r, yerr=yerr2, color='blue', barsabove='False', linestyle='none', linewidth=0.7) 
     pyplot.rc('text', usetex=True)
     pyplot.xlabel(r'$\tan{\beta}$')
     pyplot.ylabel(r'r = $\frac{signal}{95\%CL \ limit\ on\ signal}$')
@@ -112,9 +112,9 @@ def PlottingTanBr2_includingHiggs(tanB, r, saveTitle):
 
 def PlottingTanBr3_noHiggs(tanB, r, saveTitle):
     fig, ax = pyplot.subplots()
-    pyplot.plot(tanB, r, color='blue', marker='o', markersize=8, linestyle='dashed', linewidth=1)
+    pyplot.plot(tanB, r, color='blue', marker='o', markersize=7, linestyle='dashed', linewidth=1)
     yerr2 = r*87/1000
-    ax.errorbar(tanB, r, yerr=yerr2, color='blue', barsabove='False')
+    ax.errorbar(tanB, r, yerr=yerr2, color='blue', barsabove='False', linestyle='none', linewidth=0.7)
     pyplot.rc('text', usetex=True)
     pyplot.xlabel(r'$\tan{\beta}$')
     pyplot.ylabel(r'r = $\frac{signal}{95\%CL \ limit\ on\ signal}$')
